05a47852556f63913825979b88bb5eb534d05657
poppywis3 <jennbirch@gmail.com>
2015-05-17 08:24:26 -0700

    adding jen week 2 assignment

---
+++ jen/jen_week2_translate.py
@@ -0,0 +1,15 @@
+
+# week two assignment - jen b
+
+user_text = raw_input("Enter some text: ")
+
+user_text = user_text.replace("a", "4")
+user_text = user_text.replace("b", "8")
+user_text = user_text.replace("e", "3")
+user_text = user_text.replace("l", "1")
+user_text = user_text.replace("o", "0")
+user_text = user_text.replace("s", "5")
+user_text = user_text.replace("t", "7")
+
+print user_text
+
