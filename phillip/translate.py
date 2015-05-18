ae90f06ad54600e807285e40bc3fbeb80dc0efbe
Phillip Gray <escapethegray11235@gmail.com>
2015-05-17 22:51:38 -0400

    finished script without looping

--- phillip/translate.py
+++ phillip/translate.py
@@ -1,17 +1,3 @@
-translation_dict = {
-    "a": "4",
-    "b": "8",
-    "e": "3",
-    "l": "1",
-    "o": "0",
-    "s": "5",
-    "t": "7"
-}
-
 original_text = raw_input("Enter some text: ")
-new_string = ""
-
-for char in original_text:
-    if char in translation_dict.keys():
-
-        new_string += char
+new_text = original_text.replace("a", "4").replace("b", "8").replace("e", "3").replace("l", "1").replace("o", "0").replace("s", "5"). replace("t", "7")
+print new_text
